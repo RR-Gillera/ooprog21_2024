c46c013f7686bf386196d77bc5d2d95a46584d84
GxRuss <russellgillera19@gmail.com>
2024-08-14 21:36:58 +0800

    Added 3 java programs

---
+++ Chapter 1/MovieQuoteInfor.java
@@ -0,0 +1,13 @@
+public class MovieQuoteInfo {
+    public static void main(String[] args) {
+        String quote = "Sometimes good guys gotta do bad things to make the bad guys pay.";
+        String movie = "Suits";
+        String character = "Mike Ross";
+        int year = 2011;
+
+        System.out.println("Movie Quote: \"" + quote + "\"");
+        System.out.println("Movie: " + movie);
+        System.out.println("Character: " + character);
+        System.out.println("Year: " + year);
+    }
+}
---
+++ Chapter 1/TableAndChairs.java
@@ -0,0 +1,10 @@
+public class TableAndChairs {
+    public static void main(String[] args) {
+        System.out.println("\t X                        X");
+        System.out.println("\t X                        X");
+        System.out.println("\t X       XXXXXXXXXX       X");
+        System.out.println("\t XXXXX   X        X   XXXXX");
+        System.out.println("\t X   X   X        X   X   X");
+        System.out.println("\t X   X   X        X   X   X");
+    }
+}
