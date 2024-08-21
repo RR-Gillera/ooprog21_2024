d842578bf4c29786deefb231f861f4eb85a7f17c
GxRuss <russellgillera19@gmail.com>
2024-08-21 21:06:24 +0800

    Updated

--- Chapter 1/SwingAlert.java
+++ Chapter 1/SwingAlert.java
@@ -2,7 +2,6 @@ import javax.swing.JOptionPane;
 
 public class SwingAlert {
     public static void main(String[] args) {
-        // Display a simple message dialog
         JOptionPane.showMessageDialog(null, "Hello World!");
     }
 }
