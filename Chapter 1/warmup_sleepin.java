58472fc3efa4a324d7baf345351d8fe6b8d3d3b3
GxRuss <russellgillera19@gmail.com>
2024-08-28 20:13:36 +0800

    Added new warmup Activity

---
+++ Chapter 1/warmup_sleepin.java
@@ -0,0 +1,14 @@
+public class warmup_sleepin {
+    
+    public static void main(String[] args) {
+        System.out.println(sleepIn(false, false));
+        System.out.println(sleepIn(true, false));
+        System.out.println(sleepIn(false, true)); 
+        System.out.println(sleepIn(true, true)); 
+    }
+
+    public static boolean sleepIn(boolean weekday, boolean vacation) {
+        return !weekday || vacation;
+    }
+
+}
