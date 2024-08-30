640543e469516bd7b15a407982a4bfc33c437b4b
Russell Ray Gillera <145899803+RR-Gillera@users.noreply.github.com>
2024-08-30 08:57:34 +0700

    Update warmup_sleepin.java

--- Chapter 1/warmup_sleepin.java
+++ Chapter 1/warmup_sleepin.java
@@ -1,14 +1,26 @@
+import java.util.Scanner;
+
 public class warmup_sleepin {
-    
+
     public static void main(String[] args) {
-        System.out.println(sleepIn(false, false));
-        System.out.println(sleepIn(true, false));
-        System.out.println(sleepIn(false, true)); 
-        System.out.println(sleepIn(true, true)); 
+        Scanner s = new Scanner(System.in);
+        
+        
+        System.out.print("Enter 'true' if weekday and 'false' if not: ");
+        String weekdayInput = s.nextLine();
+        boolean weekday = Boolean.parseBoolean(weekdayInput);
+        
+        System.out.print("Enter 'true' if in vacation, 'false' if not: ");
+        String vacationInput = s.nextLine();
+        boolean vacation = Boolean.parseBoolean(vacationInput);
+        
+        System.out.println("Can you sleep in?: " + sleepIn(weekday, vacation));
+        
+        
+        s.close();
     }
 
     public static boolean sleepIn(boolean weekday, boolean vacation) {
         return !weekday || vacation;
     }
-
 }
