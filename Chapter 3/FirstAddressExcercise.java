80ba5f19a1d98bab563062ab140535a3bb3d94dd
unknown <OO222@ccs.local>
2024-09-21 00:32:18 +0700

    new assignment

---
+++ Chapter 3/FirstAddressExcercise.java
@@ -0,0 +1,16 @@
+public class FirstAddressExcercise{
+
+    public static void main(String[] args){
+        
+        FirstAddressExcercise obj = new FirstAddressExcercise();
+        obj.displayAddress();
+
+    }
+
+    public void displayAddress(){
+        System.out.println("\n\t\tSamsung Electronics Co., Ltd.");
+        System.out.println("\t\tSamsung Town, 129 Samsung-ro, Yeongtong-gu");
+        System.out.println("\t\tSuwon-si, Gyeonggi-do, 16677");
+        System.out.println("\t\tSouth Korea");
+    }
+}
