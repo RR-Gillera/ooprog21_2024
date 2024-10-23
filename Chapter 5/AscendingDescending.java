ab334934d1047986a004ba1924de1b07aebefbe3
GxRuss <russellgillera19@gmail.com>
2024-10-23 21:57:50 +0800

    New Chapter 5 Activity

---
+++ Chapter 5/AscendingDescending.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class AscendingDescending {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        System.out.print("\tEnter the first number: ");
+        int num1 = s.nextInt();
+        System.out.print("\tEnter the second number: ");
+        int num2 = s.nextInt();
+        System.out.print("\tEnter the third number: ");
+        int num3 = s.nextInt();
+
+        System.out.print("\n\t\tNumbers in ascending order: ");
+        if (num1 <= num2 && num1 <= num3) {
+            if (num2 <= num3) {
+                System.out.println(num1 + " " + num2 + " " + num3);
+            } else {
+                System.out.println(num1 + " " + num3 + " " + num2);
+            }
+        } else if (num2 <= num1 && num2 <= num3) {
+            if (num1 <= num3) {
+                System.out.println(num2 + " " + num1 + " " + num3);
+            } else {
+                System.out.println(num2 + " " + num3 + " " + num1);
+            }
+        } else {
+            if (num1 <= num2) {
+                System.out.println(num3 + " " + num1 + " " + num2);
+            } else {
+                System.out.println(num3 + " " + num2 + " " + num1);
+            }
+        }
+
+        System.out.print("\t\tNumbers in descending order: ");
+        if (num1 >= num2 && num1 >= num3) {
+            if (num2 >= num3) {
+                System.out.println(num1 + " " + num2 + " " + num3);
+            } else {
+                System.out.println(num1 + " " + num3 + " " + num2);
+            }
+        } else if (num2 >= num1 && num2 >= num3) {
+            if (num1 >= num3) {
+                System.out.println(num2 + " " + num1 + " " + num3);
+            } else {
+                System.out.println(num2 + " " + num3 + " " + num1);
+            }
+        } else {
+            if (num1 >= num2) {
+                System.out.println(num3 + " " + num1 + " " + num2);
+            } else {
+                System.out.println(num3 + " " + num2 + " " + num1 + "/n/n");
+            }
+        }
+    }
+}
