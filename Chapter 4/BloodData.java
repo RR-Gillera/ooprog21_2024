d0cbf2bea2028813986fed435a5f32746c245043
Russell Ray Gillera <145899803+RR-Gillera@users.noreply.github.com>
2025-02-11 19:13:35 +0800

    Update BloodData.java

--- Chapter 4/BloodData.java
+++ Chapter 4/BloodData.java
@@ -1,40 +1,29 @@
-public class BloodData {
-    private String bloodType;
-    private String rhFactor;
-
-    public BloodData() {
-        this.bloodType = "O";
-        this.rhFactor = "+";
-    }
-
-    public BloodData(String bloodType, String rhFactor) {
-        this.bloodType = bloodType;
-        this.rhFactor = rhFactor;
-    }
-
-    public void setBloodType(String bloodType) {
-        this.bloodType = bloodType;
-    }
-
-    public void setRhFactor(String rhFactor) {
-        this.rhFactor = rhFactor;
-    }
-
-    public String getBloodType() {
-        return bloodType;
-    }
-
-    public String getRhFactor() {
-        return rhFactor;
-    }
-
-    public void defaultPatient() {
-        this.bloodType = "A";
-        this.rhFactor = "-";
-    }
-
-    public void displayBloodInfo() {
-        System.out.println("Your blood type is: " + this.bloodType);
-        System.out.println("Your Rh Factor is: " + this.rhFactor);
-    }
+public class BloodData{
+   private String bloodType;
+   private String rhFactor;
+   
+   BloodData(){
+      this.bloodType = "O";
+      this.rhFactor = "+";
+   }
+   
+   BloodData(String blType, String rFactor){
+      bloodType = blType;
+      rFactor = rFactor;   
+   }
+   
+   public void setBloodType(String blType){
+      bloodType = blType;
+   }
+   
+   String getBloodType(){
+      return bloodType;
+   }
+   
+   public void setRhFactor(tring rFactor){\
+      rFactor = rFactor;
+   }
+   
+   String getRhFactor(){
+      return rhFactor;
 }
